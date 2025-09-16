0441a3debd1d9c0c96c3aa860db89048da51ecb0
MALR07 <miguelangelledesma5b@gmail.com>
2025-09-16 19:34:46 +0200

    Release 1

--- superior-star/.netlify/build/entry.mjs
+++ superior-star/.netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_BOZvcXBe.mjs';
+import { manifest } from './manifest_BHZo6WBF.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -18,7 +18,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "deb84762-01bc-45c1-b82d-1a0c12ef4266"
+    "middlewareSecret": "2eae6d60-cfb4-4c73-8f43-09b869c17041"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
